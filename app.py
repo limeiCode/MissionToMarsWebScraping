10f8e9e289cfdde11b44397c5d9ad9fe35f35c72
Limei Hou <limei_hou@hotmail.com>
2019-10-21 19:46:26 -0400

    index.html done

--- app.py
+++ app.py
@@ -1,23 +1,28 @@
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
-import scrape_MissionToMars
+import scrape_mars
 
 # Create an instance of Flask
 app = Flask(__name__)
 
 # Use PyMongo to establish Mongo connection
-mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_db")
+# # Use flask_pymongo to set up mongo connection
+# app.config["MONGO_URI"] = "mongodb://localhost:27017/craigslist_app"
+# mongo = PyMongo(app)
 
+# Create a root route / that will query your Mongo database and pass the mars data into an HTML template to display the data.
+# Use Pymongo for CRUD applications for your database. For this homework, you can simply overwrite the existing document each time the /scrape url is visited and new data is obtained.
 
 # Route to render index.html template using data from Mongo
 @app.route("/")
 def home():
 
-    # Find one record of data from the mongo database
-    destination_data = mongo.db.collection.find_one()
-
+    # Find one record of data from the mongo database collection mars
+    marsdata_mgl = mongo.db.mars.find_one()
+    # teams = list(db.team.find())
     # Return template and data
-    return render_template("index.html", marsdata=destination_data)
+    return render_template("index.html", marsdata_html=marsdata_mgl)
 
 
 # Route that will trigger the scrape function
@@ -25,10 +30,10 @@ def home():
 def scrape():
 
     # Run the scrape function
-    mars_data = scrape_MisstionToMars.scrape_info()
+    marsdata_scrp = scrape_mars.scrape()
 
     # Update the Mongo database using update and upsert=True
-    mongo.db.collection.update({}, mars_data, upsert=True)
+    mongo.db.mars.update({}, marsdata_scrp, upsert=True)
 
     # Redirect back to home page
     return redirect("/")
--- scrape_mars.py
+++ scrape_mars.py
@@ -1,4 +1,6 @@
-from selenium import webdriver
+# Dependencies: import necessary libraries
+from selenium import webdriver  # Web Scraping Framework # selenium webdriver API
+from selenium.common.exceptions import NoSuchElementException
 from bs4 import BeautifulSoup as bs
 import time
 
@@ -8,21 +10,64 @@ def init_browser():
     return webdriver.Chrome("windows/chromedriver")
 
 
-def scrape_info():
+def scrape():
     browser = init_browser()
-
-    # Visit visitcostarica.herokuapp.com
-    url = "https://..."
+    marsdata_scrp = {}
+    # <1>NASA Mars News:
+    # Scrape the NASA Mars News Site and collect the {latest} News [Title] and [Paragraph Text].
+    # Assign the text to variables that you can reference later.
+    url = 'https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest'
     browser.get(url)
     time.sleep(1)
     # Scrape page into Soup
     html = browser.page_source
     soup = bs(html, "html.parser")
+    # find() is the later one, find_all() is all
+    news_list = soup.find_all('div', class_='list_text')
+    latestnews_title = news_list[0].find('div', class_='content_title').a.text
+    latestnews_p = news_list[0].find('div', class_='article_teaser_body').text
+    marsdata_scrp["latestnews_title"] = latestnews_title
+    marsdata_scrp["latestnews_p"] = latestnews_p
 
-    ...
+
+    # <2> JPL Mars Space Images - Featured Image :
+    # Use Selenium to navigate the site and find the image url for the {current Featured Mars Image} and assign the [url string] to a variable called featured_image_url.
+    # Make sure to find the image url to the {full size .jpg image}.
+    # Make sure to save a complete url string for this image.
+    url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+    browser.get(url)
+
+    # Scrape page into Soup
+    html = browser.page_source
+    # Create BeautifulSoup object; parse with 'html.parser'
+    soup = bs(html, 'html.parser')
+    # Examine the results, then determine element that contains sought info
+    # print(soup.body.prettify())
+    imageurl_list = soup.find_all('footer')
+    featured_image_url = 'https://www.jpl.nasa.gov' + \
+        imageurl_list[0].find('a', class_='button fancybox')['data-fancybox-href']
+    marsdata_scrp["featured_image_url"] = featured_image_url
+
+    # <3> Mars Weather
+    # scrape the {latest Mars weather tweet} from the page. Save the tweet text for the weather report as a variable called mars_weather.
+    
+    # URL of page to be scraped
+    url = 'https://twitter.com/marswxreport?lang=en'
+    browser.get(url)
+    # Scrape page into Soup
+    html = browser.page_source
+    # Create BeautifulSoup object; parse with 'html.parser'
+    soup = bs(html, 'html.parser')
+    # Examine the results, then determine element that contains sought info
+    # print(soup.body.prettify())
+    tweet_list = soup.find_all('div',class_='js-tweet-text-container')
+    latestmarsweather_tweet = tweet_list[0].find('p').text
+    marsdata_scrp["latestmarsweather_tweet"] = latestmarsweather_tweet
+
+
 
     # Close the browser after scraping
     browser.close()
 
     # Return results
-    return mars_data
+    return marsdata_scrp
